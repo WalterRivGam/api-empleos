25452e97b57c50883ce400179ea6a4cdef99e647
Walter Rivera Gambini <walterivgam@gmail.com>
2025-10-03 23:09:33 -0500

    Expiración de vacantes

--- src/main/java/com/api/apiempleos/service/impl/VacanteServiceImpl.java
+++ src/main/java/com/api/apiempleos/service/impl/VacanteServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.api.apiempleos.entity.Vacante;
@@ -48,6 +49,24 @@ public class VacanteServiceImpl implements VacanteService {
         vacante.setEstado("activa");
         vacante.setCreatedAt(LocalDateTime.now());
         return vacanteRepository.save(vacante);
+    }
+
+    /**
+     * Expiración de vacantes
+     * Se ejecuta todos los días a la 1 a.m.
+     * Actualiza el estado de todas las vacantes activas cuya fecha de vencimiento
+     * es anterior al día actual a 'cerrada' en la base de datos.
+     */
+    @Scheduled(cron = "0 0 1 * * ?")
+    public void expirarVacante() {
+        System.out.println("Expirar vacantes");
+        obtenerVacantesActivas().forEach(vacante -> {
+            if (vacante.getEstado().equalsIgnoreCase("activa")
+                    && vacante.getFechaVencimiento().isBefore(LocalDate.now())) {
+                vacante.setEstado("cerrada");
+                vacanteRepository.save(vacante);
+            }
+        });
     }
 
 }
